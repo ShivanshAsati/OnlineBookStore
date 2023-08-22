2438f9af848885d3a0707e03d2e0f4a7b692dd0f
dipammokal <dipammokal007@gmail.com>
2023-08-22 11:41:38 +0530

    address crud completed

--- src/main/java/com/bookstore/controller/AddressController.java
+++ src/main/java/com/bookstore/controller/AddressController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,7 @@ public class AddressController {
 	
 	@PostMapping("/addtouser/{userId}")
 	public ResponseEntity<?> addressDetails(@PathVariable Long userId, @RequestBody AddressDTO addressDTO) {
-		return ResponseEntity.status(HttpStatus.OK).body(addressService.addressDetails(userId, addressDTO));
+		return ResponseEntity.status(HttpStatus.OK).body(addressService.addAddress(userId, addressDTO));
 	}
 	
 	@GetMapping("/user/{userId}")
@@ -31,9 +32,14 @@ public class AddressController {
 		return ResponseEntity.status(HttpStatus.FOUND).body(addressService.getAddress(userId));
 	}
 	
-	@DeleteMapping("/userid/addressid/{userId}/{addressId}")
-	public ResponseEntity<?> deleteAddress(@PathVariable Long userId, @PathVariable Long addressId) {
-		return ResponseEntity.status(HttpStatus.FOUND).body(addressService.deleteAddress(userId, addressId));
+	@DeleteMapping("/addressid/{addressId}")
+	public ResponseEntity<?> deleteAddress(@PathVariable Long addressId) {
+		return ResponseEntity.status(HttpStatus.FOUND).body(addressService.deleteAddress(addressId));
+	}
+	
+	@PutMapping("/addressid{addressId}")
+	public ResponseEntity<?>  updateAddress(@PathVariable Long addressId, @RequestBody AddressDTO addressDTO) {
+		return ResponseEntity.status(HttpStatus.FOUND).body(addressService.updateAddress(addressId, addressDTO));
 	}
 	
 
--- src/main/java/com/bookstore/service/AddressService.java
+++ src/main/java/com/bookstore/service/AddressService.java
@@ -5,12 +5,12 @@ import java.util.List;
 import com.bookstore.dto.AddressDTO;
 import com.bookstore.dto.ApiResponse;
 import com.bookstore.dto.DetachedAddressDTO;
-import com.bookstore.entities.Address;
 
 public interface AddressService 
 {
-	public ApiResponse addressDetails(Long userId, AddressDTO addressDTO);
+	public ApiResponse addAddress(Long userId, AddressDTO addressDTO);
 //	public List<AddressDTO> getAddress(Long userId);
 	public List<DetachedAddressDTO> getAddress(Long userId);
-	public ApiResponse deleteAddress(Long userId, Long addressId);
+	public ApiResponse deleteAddress(Long addressId);
+	public ApiResponse updateAddress(Long addressId, AddressDTO addressDTO);
 }
