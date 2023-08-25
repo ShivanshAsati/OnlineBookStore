943a7a9a1c6ff3a80ee86bbd3420a40cf2199a1a
Shivansh <asatishivanshshiv@gmail.com>
2023-08-26 00:08:19 +0530

    added cart.jsx cart.css

--- Project/Frontend/book-store-react/src/components/book.jsx
+++ Project/Frontend/book-store-react/src/components/book.jsx
@@ -11,9 +11,6 @@ function BookGallery()
     const [books,setBooks] = useState([]);
     
     const navigate = useNavigate();
-    
-    
-   
 
     useEffect(()=>{
         selectBooks();
---
+++ Project/Frontend/book-store-react/src/services/cartService.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+import {createUrl,log} from '../utils/utils';
+
+export async function addCartItem(){
+
+}
