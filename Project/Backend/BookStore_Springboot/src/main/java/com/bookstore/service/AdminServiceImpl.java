2ece967bed097c3834cac7f18519b4f76b149e2b
PiyushKB27 <piyushbokade27@gmail.com>
2023-08-30 19:16:00 +0530

    Admin backend

---
+++ Project/Backend/BookStore_Springboot/src/main/java/com/bookstore/service/AdminServiceImpl.java
@@ -0,0 +1,11 @@
+package com.bookstore.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class AdminServiceImpl implements AdminService {
+
+}
